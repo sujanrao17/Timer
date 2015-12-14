4373e9688d296effd4e23386bb3e454043f9c135
Sujan <sujanrao17@gmail.com>
2015-12-14 15:24:19 -0600

    added handler and alert dialog

--- app/src/main/java/main/coderindigo/com/countdown/activity/MainActivity.java
+++ app/src/main/java/main/coderindigo/com/countdown/activity/MainActivity.java
@@ -1,17 +1,37 @@
 package main.coderindigo.com.countdown.activity;
 
+
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import main.coderindigo.com.countdown.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Dialog.OnClickListener{
+
+    private TextView mTime;
+    private Button startButton;
+    private Button stopButton;
+    private Handler handler;
+    private long timeRemaining = 5000;
+
+    public MainActivity() {
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,54 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        mTime = (TextView) findViewById(R.id.time);
+        startButton = (Button) findViewById(R.id.start_button);
+        stopButton = (Button) findViewById(R.id.stop_button);
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+
+        handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                timeRemaining = timeRemaining- 1000;
+                if (timeRemaining>0){
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+
+
+
+
+
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.start_button){
+            Toast.makeText(MainActivity.this, "ON", Toast.LENGTH_SHORT).show();
+            LayoutInflater inflater = LayoutInflater.from(this);
+            View view = inflater.inflate(R.layout.user_input, null);
+            EditText userInput = (EditText) view.findViewById(R.id.user_input);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Enter Time");
+            builder.setView(R.layout.user_input);
+            builder.setPositiveButton("OK", this);
+            builder.setNegativeButton("CANCEL", this);
+            builder.show();
+
+
+        }else if(v.getId() == R.id.stop_button){
+            Toast.makeText(MainActivity.this, "OFF", Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 
     @Override
@@ -50,5 +118,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which){
+            case DialogInterface.BUTTON_POSITIVE:
+
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                break;
+
+        }
+
     }
 }
